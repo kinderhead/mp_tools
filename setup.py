fe2dd952b4d9d6378e776d102d7282b227033416
Fred <kinderhead@icloud.com>
2020-12-12 15:35:25 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 15:31:40 2020
+
+@author: Daniel
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mp_tools_kinderhead",
+    version="1.0.0",
+    author="kinderhead",
+    description="multiprocessing tools",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6'
+    )
